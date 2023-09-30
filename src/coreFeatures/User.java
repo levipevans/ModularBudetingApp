883d0a44560ec0d2db39e93badf09fd52ef4395c
River <gseeber5@gmail.com>
2023-09-30 14:45:45 -0700

    Add user basics Added:   - Constructor (taking int for category size, or none to default to 25)   - Member fields for categories, spending per category, and an array of accounts   - The constructor will create one checkings and one savings account, both with an initial       balance of zero (0).   - Getters for the array of categories, spendingByCategory, and accounts

---
+++ src/coreFeatures/User.java
@@ -0,0 +1,49 @@
+package coreFeatures;
+
+import java.math.BigDecimal;
+
+public class User {
+    private String[] categories = new String[] {"Food", "Snacks and self regret purchases", "Gas", "Entertainment", "Housing", "Subscriptions"};
+
+    private BigDecimal[] spendingByCategory = new BigDecimal[categories.length];
+    
+    private Account[] accounts;
+
+    //set the category size and then make a checkings and savings account
+    public User(int categorySize){
+        categorySetSize(categorySize);
+
+        accounts = new Account[2];
+        accounts[0] = new Account("Checking", BigDecimal.ZERO, false);
+        accounts[1] = new Account("Savings", BigDecimal.ZERO, true);
+    }
+
+    public User(){
+        //25 default value
+        this(25);
+    }
+
+    public Account[] geAccounts(){
+        return accounts;
+    }
+
+    public String[] getCategories(){
+        return categories;
+    }
+
+    public void categorySetSize(int num){
+        //save categories and spendingByCategories into buffs
+        String[] catBuff = categories;
+        categories = new String[num];
+        BigDecimal[] spendBuff = spendingByCategory;
+        spendingByCategory = new BigDecimal[num];
+
+        for(int i = 0; i < catBuff.length && i < spendBuff.length; ++i){
+            categories[i] = catBuff[i];
+            spendingByCategory[i] = spendBuff[i];
+        }
+    }
+
+
+
+}
