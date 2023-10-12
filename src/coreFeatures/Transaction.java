0aa3163ec66c7f5f49e74e45aa1a0410c41e41a1
Levi Evans <levipierceevans@gmail.com>
2023-10-11 19:02:48 -0700

    getters/setters

--- src/coreFeatures/Transaction.java
+++ src/coreFeatures/Transaction.java
@@ -16,4 +16,25 @@ public class Transaction {
     private LocalDate date;
     private String description;
     
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+    
 }
